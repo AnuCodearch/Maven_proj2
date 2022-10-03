bb5c0a2a96448eef478b2fd399128152c611f642
anuCodearch <anubobhate@gmail.com>
2022-10-03 12:32:52 +0530

    message for commit 2.

--- src/test/java/testclasses/TC03.java
+++ src/test/java/testclasses/TC03.java
@@ -2,4 +2,6 @@ package testclasses;
 
 public class TC03 {
 public void m1() {System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");}
+public void m2() {System.out.println("Heyyyyyyyyy");}
+public void m3() {System.out.println("Hellooooooooooooo");}
 }
