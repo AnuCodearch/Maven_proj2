73691158af1926592903edbe5b9c9508794c27a0
anuCodearch <anubobhate@gmail.com>
2022-10-03 17:07:57 +0530

    commit 1 message

--- src/test/java/testclasses/TC03.java
+++ src/test/java/testclasses/TC03.java
@@ -5,5 +5,6 @@ public void m1() {System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");}
 public void m2() {System.out.println("Heyyyyyyyyy");}
 public void m3() {System.out.println("Hellooooooooooooo");}
 public void m4() {System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");}
+public void m5() {System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");}
 
 }
